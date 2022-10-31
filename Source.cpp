40bf18c7043ae27794a7420d311a49b5632d1b22
Ilikeall <klutiy72@gmail.com>
2022-10-31 19:28:44 +0100

    added class Shader

--- Source.cpp
+++ Source.cpp
@@ -1,6 +1,8 @@
 #include<glad/glad.h>
 #include<GLFW/glfw3.h>
 #include<iostream>
+#include<fstream>
+#include"Shader.h"
 
 struct Color
 {
@@ -21,6 +23,8 @@ void processInput(GLFWwindow* win)
 		background = { 0.0f, 0.0f, 1.0f, 1.0f };
 	if (glfwGetKey(win, GLFW_KEY_4) == GLFW_PRESS)
 		background = { 0.0f, 0.8f, 0.7f, 1.0f };
+	if (glfwGetKey(win, GLFW_KEY_5) == GLFW_PRESS)
+		background = { 0.0f, 0.0f, 0.0f, 1.0f };
 }
 
 void onResize(GLFWwindow* win, int width, int height)
@@ -30,12 +34,13 @@ void onResize(GLFWwindow* win, int width, int height)
 
 int main()
 {
+#pragma region WINDOW INITIALIZATION
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	GLFWwindow* win = glfwCreateWindow(1600, 900, "My OpenGL Window", NULL, NULL);
+	GLFWwindow* win = glfwCreateWindow(900, 900, "My OpenGL Window", NULL, NULL);
 	if (win == NULL)
 	{
 		std::cout << "Error.Couldn't create window!" << std::endl;
@@ -55,19 +60,75 @@ int main()
 	std::cout << "OpenGL version: " << glGetString(GL_VERSION) << std::endl;
 
 	glfwSetFramebufferSizeCallback(win, onResize);
+#pragma endregion
 
-	glViewport(0, 0, 1600, 900);
+	glViewport(0, 0, 900, 900);
+
+#pragma region POLIGON INITIALIZATION
+
+	const int verts = 4;
+
+	float polygon[verts * (3 + 3)] = {
+		   0.0f, 0.5f, 0.0f,    1.0f, 0.0f, 1.0f,
+			0.5f, 0.0f, 0.0f,    1.0f, 0.0f, 0.0f,
+		   -0.5f, 0.0f, 0.0f,    0.0f, 1.0f, 0.0f,
+		  0.0f, -0.5f, 0.0f,    0.0f, 0.0f, 1.0f
+
+	};
+
+	unsigned int vertexId[] = {
+		0, 1, 2,
+		1, 2, 3
+
+	};
+
+	unsigned int VBO_polygon, VAO_polygon, EBO_polygon;
+	glGenBuffers(1, &VBO_polygon);
+	glGenBuffers(1, &EBO_polygon);
+	glGenVertexArrays(1, &VAO_polygon);
+
+	glBindVertexArray(VAO_polygon);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO_polygon);
+	glBufferData(GL_ARRAY_BUFFER, verts * 6 * sizeof(float), polygon, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO_polygon);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), vertexId, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+
+	Shader* polygon_shader = new Shader("Shaders\\v_sh.vert", "Shaders\\f_sh.frag");
+
+#pragma endregion
+
+	float position[3] = { 1.0f, 0.0f, 1.0f };
+
+	int stage = 0;
 
 	while (!glfwWindowShouldClose(win))
 	{
+		position[0] = sin(glfwGetTime() * 2);
+		position[1] = cos(glfwGetTime() * 2);
+		position[2] = 0;
+
 		processInput(win);
 
 		glClearColor(background.r, background.g, background.b, background.a);
 		glClear(GL_COLOR_BUFFER_BIT);
 
+		polygon_shader->use();
+		polygon_shader->setFloatVec("uniformPos", position, 3);
+
+		glBindVertexArray(VAO_polygon);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
 		glfwSwapBuffers(win);
 		glfwPollEvents();
 	}
+
+	delete polygon_shader;
 
 	glfwTerminate();
 	return 0;
